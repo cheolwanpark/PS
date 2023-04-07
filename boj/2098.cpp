db587dee3eae178e850b14ca779cc953d35c177f
CodingVillain <cheolwan.park552@gmail.com>
2023-04-07 07:58:35 +0000

    fix 2098, improve performance

--- boj/2098.cpp
+++ boj/2098.cpp
@@ -11,24 +11,24 @@ constexpr int fullmask(int N) {
 int N;
 int W[16][16];
 
-int memo[16][16][fullmask(16)]={0,};
-int solve(int start, int cur, int mask) { // start에서 시작해서 cur에 도착하면서 cur 이전에 mask만큼의 도시를 거친 비용중 최솟값
-  if(start == cur) {
+int memo[16][fullmask(16)]={0,};
+int solve(int cur, int mask) {
+  if(cur == 0) {
     return mask == 0 ? 0 : INF;
   } else if(mask == 0) {
     return INF;
-  } else if(memo[start][cur][mask] > 0) {
-    return memo[start][cur][mask];
+  } else if(memo[cur][mask] > 0) {
+    return memo[cur][mask];
   }
   
-  memo[start][cur][mask] = INF;
+  memo[cur][mask] = INF;
   for(int i=0; i<N; ++i) {
     if(((1 << i) & mask) && W[i][cur] > 0) {
-      memo[start][cur][mask] = min(memo[start][cur][mask], solve(start, i, mask & (~(1 << i))) + W[i][cur]);
+      memo[cur][mask] = min(memo[cur][mask], solve(i, mask & (~(1 << i))) + W[i][cur]);
     }
   }
-  memo[start][cur][mask] = min(memo[start][cur][mask], INF);
-  return memo[start][cur][mask];
+  memo[cur][mask] = min(memo[cur][mask], INF);
+  return memo[cur][mask];
 }
 
 int main() {
@@ -39,14 +39,10 @@ int main() {
 
   int mn = INF;
   for(int i=0; i<N; ++i) {
-    for(int j=0; j<N; ++j) {
-      if(W[j][i] > 0) {
-        int mask = (~(1 << j));
-        mn = min(mn, solve(i, j, fullmask(N) & mask) + W[j][i]);
-      }
+    if(W[i][0] > 0) {
+      int mask = (~(1 << i));
+      mn = min(mn, solve(i, fullmask(N) & mask) + W[i][0]);
     }
   }
   printf("%d\n", mn);
 }
-
-// 반례: https://www.acmicpc.net/board/view/51533
